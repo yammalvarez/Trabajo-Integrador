2a917fb6a7f9d03c0f5ac3d1f43e7aea6632272f
yammalvarez <155074579+yammalvarez@users.noreply.github.com>
2023-12-28 09:21:08 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+var pasoActual = 1;
+const ULTIMO_PASO = 6;
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const btnSiguiente = document.getElementById("btn-siguiente");
+
+    btnSiguiente.addEventListener('click', avanzarPaso);
+
+});
+
+function avanzarPaso(){
+    const nombrePasoActual = "paso-" + pasoActual;
+
+    const divActual = document.getElementById(nombrePasoActual);
+    divActual.style = "display: none";
+
+    pasoActual++;
+
+    const nuevoNombrePasoActual = "paso-" + pasoActual;
+    const nuevoDivActual = document.getElementById(nuevoNombrePasoActual);
+    nuevoDivActual.style.display = "";
+
+    if(pasoActual == ULTIMO_PASO){
+        deshabilitarBotonSiguiente();
+        prepararResumen();
+    }
+}
+
+function deshabilitarBotonSiguiente(){
+    const btnSiguiente = document.getElementById("btn-siguiente");
+    btnSiguiente.disabled = true;
+}
+
+function prepararResumen() {
+    const resumenNombre = document.getElementById("resumen-nombre");
+    const resumenFecha = document.getElementById("resumen-fecha");
+    const resumenHora = document.getElementById("resumen-hora");
+    const resumenServicio = document.getElementById("resumen-servicio");
+    const resumenTelefono = document.getElementById("resumen-telefono");
+
+    resumenNombre.innerText = "Nombre: " + document.getElementById("nombre").value;
+    resumenFecha.innerText = "Fecha: " + document.getElementById("String: "+fecha.toString()).value;
+    resumenHora.innerText = "Hora: " + document.getElementById("hora").value;
+    resumenServicio.innerText = "Servicio: " + document.getElementById("servicio").value;
+    resumenTelefono.innerText = "Telefono: " + document.getElementById("telefono").value;
+}
+/*document.getElementById('filtrar').addEventListener('click', function(){
+    let nombreAFiltrar = document.getElementById('filtro-nombre').value;
+    let todosLosTrs = document.querySelectorAll('tr');
+    todosLosTrs.forEach((unTr) => {
+     let trValido = false
+     let children = unTr.children;
+     for(let i= 0; i < children.length; i ++){
+        let elemento = children[i];
+        if(elemento.innerText == nombreAFiltrar){
+             trValido = true
+         }
+        }
+        if(! trValido){
+            unTr.style.display = 'none';
+        }
+    })
+})*/
